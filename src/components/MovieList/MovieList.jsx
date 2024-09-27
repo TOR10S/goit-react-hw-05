e3ab136bae7db2f41a73a6197d607778322ac367
TOR10S <andriivoro@gmail.com>
2024-09-27 17:34:18 +0300

    ready

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,7 +6,11 @@ export default function MovieList({movies}) {
     return (
         <ul>
             {movies.map((movie) => {
-                return <li key={movie.id}><Link to={`/movies/${movie.id.toString()}`} state={{ from: location.pathname }}>{movie.title}</Link></li>
+                console.log(location.pathname);
+                console.log(location);
+                
+                
+                return <li key={movie.id}><Link to={`/movies/${movie.id.toString()}`} state={{ from: location }}>{movie.title}</Link></li>
             })}
         </ul>
     )
